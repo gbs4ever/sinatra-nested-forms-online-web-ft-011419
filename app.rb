f4a0ff7eb61ddc1e721fa872f508c0f5cd8ad29d
Sim Greenbaum <gbs4ever2@gmail.com>
2019-02-28 04:09:42 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -3,10 +3,13 @@ require './environment'
 module FormsLab
   class App < Sinatra::Base
 get '/' do
-erb :root
+  erb :root
 end
   get '/new' do
-erb :'pirates/new'
+    erb :'pirates/new'
+  end
+post '/new'  do
+  erb  :'pirates/show'
 end
 
 
