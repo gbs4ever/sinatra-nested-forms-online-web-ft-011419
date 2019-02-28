8dcadaf1467a6a25ee4789c5109b86a0a3621e63
Sim Greenbaum <gbs4ever2@gmail.com>
2019-02-28 03:24:45 +0000

    Automatically backed up by Learn

--- app/models/ship.rb
+++ app/models/ship.rb
@@ -1,7 +1,9 @@
 class Ship
-attr_accessor   :name,:weight,:height.
-  def initialize(name, weight,height)
-    @ =
+attr_accessor   :name, :type,:booty
+  def initialize(name,type,booty)
+    @name =name
+    @type = type
+    @booty = booty
   end
 
 end
